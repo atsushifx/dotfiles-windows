5e34d5035aa7e97722c9d2102ebc49939b8d8b48
Atsushi Furukawa <atsushifx@aglabo.com>
2021-11-26 01:48:59 +0900

    powershell初期設定スクリプトを復帰

--- .config/.settings/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/.settings/powershell/Microsoft.PowerShell_profile.ps1
@@ -10,16 +10,16 @@ function isAdmin()
 {
 	((([Security.Principal.WindowsPrincipal] `
 		[Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-    			[Security.Principal.WindowsBuiltInRole] "Administrator"` )))
+			[Security.Principal.WindowsBuiltInRole] "Administrator"` )))
 }
 
 # prompt
 function prompt()
 {
 	$isAdmin =  isAdmin
-    	$prompt = $isAdmin ? " # " :  " > "
-    	$currentPath = (Split-Path (Get-Location) -Leaf)
-    	$currentDrive =(Convert-Path \).substring(0, 1)
+	$prompt = $isAdmin ? " # " :  " > "
+	$currentPath = (Split-Path (Get-Location) -Leaf)
+	$currentDrive =(Convert-Path \).substring(0, 1)
 	$userName =$env:USERNAME
 
 	# Prompt return
