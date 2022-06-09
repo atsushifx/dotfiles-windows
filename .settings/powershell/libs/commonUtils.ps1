2faa9322cf81dd625ab3cb840a6b9bc16d0e6b75
Furukawa, Atsushi <98551+atsushifx@users.noreply.github.com>
2022-06-09 18:10:03 +0900

    mod: powershell: peco history 関数を commonUtilsに移動

--- .settings/powershell/libs/commonUtils.ps1
+++ .settings/powershell/libs/commonUtils.ps1
@@ -1,6 +1,6 @@
 <#
   .SYNOPSIS
-    freq use functions library
+    frequency use functions library
 
   .
   .NOTES
@@ -10,7 +10,9 @@
 
     Version 1.0.0
 
+THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
 #>
+Set-StrictMode -version latest
 
 
 <#
@@ -56,3 +58,29 @@ class myUserRole
   }
 }
 
+<#
+  .SYNOPSIS
+    select command from history and execute
+
+  .DESCRIPTION
+    select history wuth peco and set command line to execute this.
+
+
+  .EXAMPLE
+    Set-PSReadLineKeyHandler -chord Ctrl+p -scriptBlock { SelectandExecHistory }
+    using above that hit ctrl+p to use this.
+
+#>
+function global:SelectandExecHistory()
+{
+	$selectCmd = (tail -20 (Get-PSReadLineOption).HistorySavePath)|peco --select-1 --on-cancel error
+	if ($?) {
+		[Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+		[Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
+		[Microsoft.PowerShell.PSConsoleReadLine]::Insert($selectCmd)
+		# [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
+		# [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+	} else {
+		[Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+	}
+}
--- .settings/powershell/scripts/wzkeyconfig.ps1
+++ .settings/powershell/scripts/wzkeyconfig.ps1
@@ -11,50 +11,67 @@ THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE RISK
 #>
 
 ## Wz like key bindings
-Set-PSReadLineOption -EditMode windows
+
+<#
+  .SYNOPSIS
+    select command from history and execute
+
+  .DESCRIPTION
+    select history wuth peco and set command line to execute this.
 
 
+  .EXAMPLE
+    Set-PSReadLineKeyHandler -chord Ctrl+p -scriptBlock { SelectandExecHistory }
+    using above that hit ctrl+p to use this.
 
-## Special key function
-function global:SelectandExecHistory()
-{
-	$selectCmd = (tail -20 (Get-PSReadLineOption).HistorySavePath)|peco --select-1 --on-cancel error
-	if ($?) {
-		[Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-		[Microsoft.PowerShell.PSConsoleReadLine]::Insert($selectCmd)
-		# [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-	} else {
-		[Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+#>
+function private:wzlikekeyconfig() {
+	Set-PSReadLineOption -EditMode windows
+
+
+	# windows defaukt
+	Set-PSReadLineKeyHandler -chord Ctrl+A -function SelectAll
+	Set-PSReadLineKeyHandler -chord Ctrl+X -function cut
+	Set-PSReadLineKeyHandler -chord Ctrl+C -function copy
+
+	# exit shell
+	$keyconfig = @{
+		BriefDescription = 'exit'
+		LongDescription = 'input exit {ENTER}'
+		ScriptBlock = {
+			[Microsoft.PowerShell.PSConsoleReadLine]::Insert('exit')
+			[Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+		}
 	}
+	Set-PSReadLineKeyHandler -chord Ctrl+Z @keyconfig
+
+	# Wz/Vz like +alpha
+	Set-PSReadLineKeyHandler -chord Ctrl+a -function ShellBackwardWord
+	Set-PSReadLineKeyHandler -chord Ctrl+s -function BackwardChar
+	Set-PSReadLineKeyHandler -chord Ctrl+d -function ForwardChar
+	Set-PSReadLineKeyHandler -chord Ctrl+f -function ShellForwardWord
+
+	Set-PSReadLineKeyHandler -chord Ctrl+g -function DeleteChar
+	Set-PSReadLineKeyHandler -chord Ctrl+t -function DeleteWord
+	Set-PSReadLineKeyHandler -chord Ctrl+u -function DeleteLine
+	Set-PSReadLineKeyHandler -chord Ctrl+y -function Paste
+	Set-PSReadLineKeyHandler -chord Ctrl+j -function Paste
+
+	# history
+	$keyconfig = @{
+		BriefDescription = 'select & execute history'
+		LongDescription = 'select history with peco & execute this'
+		ScriptBlock = { SelectandExecHistory }
+	}
+
+	Set-PSReadLineKeyHandler -chord Ctrl+p @keyconfig
+	Set-PSReadLineKeyHandler -chord Ctrl+n -function NextHistory
+
+	Set-PSReadLineKeyHandler -chord Ctrl+e -function PreviousHistory
+	Set-PSReadLineKeyHandler -chord Ctrl+x -function NextHistory
 }
 
-# windows defaukt
-Set-PSReadLineKeyHandler -chord Ctrl+A -function SelectAll
-Set-PSReadLineKeyHandler -chord Ctrl+X -function cut
-Set-PSReadLineKeyHandler -chord Ctrl+C -function copy
+wzlikekeyconfig
 
-# exit shell
-Set-PSReadLineKeyHandler -chord Ctrl+Z -scriptBlock {
-	[Microsoft.PowerShell.PSConsoleReadLine]::Insert('exit')
-	[Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-}
 
-# Wz/Vz like +alpha
-Set-PSReadLineKeyHandler -chord Ctrl+a -function ShellBackwardWord
-Set-PSReadLineKeyHandler -chord Ctrl+s -function BackwardChar
-Set-PSReadLineKeyHandler -chord Ctrl+d -function ForwardChar
-Set-PSReadLineKeyHandler -chord Ctrl+f -function ShellForwardWord
-
-Set-PSReadLineKeyHandler -chord Ctrl+g -function DeleteChar
-Set-PSReadLineKeyHandler -chord Ctrl+t -function DeleteWord
-Set-PSReadLineKeyHandler -chord Ctrl+u -function DeleteLine
-Set-PSReadLineKeyHandler -chord Ctrl+y -function Paste
-Set-PSReadLineKeyHandler -chord Ctrl+j -function Paste
-
-# history
-Set-PSReadLineKeyHandler -chord Ctrl+p -scriptBlock { SelectandExecHistory }
-Set-PSReadLineKeyHandler -chord Ctrl+n -function NextHistory
-
-Set-PSReadLineKeyHandler -chord Ctrl+e -function PreviousHistory
-Set-PSReadLineKeyHandler -chord Ctrl+x -function NextHistory
 
