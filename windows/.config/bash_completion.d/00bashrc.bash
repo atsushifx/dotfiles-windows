41f367031ea7b2e79656fd71c20b629c939ce0b8
Furukawa, Atsushi <98551+atsushifx@users.noreply.github.com>
2023-03-22 08:03:06 +0900

    add: git shell: bashrcを追加

---
+++ windows/.config/bash_completion.d/00bashrc.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# @(#) : bashrc for git shell (bash)
+#
+# @version  1.0.0
+# @author   Furukawa, Atsushi <atsushifx@aglabo.com>
+# @date     2023-03-22
+# @license  MIT
+#
+
+
+echo "Exec: git shell bashrc"
+
+## EDITOR PATH
+EDITOR="/c/bin/Vim/vim90/vim.exe "
+
+export EDITOR
+
+# for VIM
+export VIMINIT=":source ~/vimfiles/vimrc"
