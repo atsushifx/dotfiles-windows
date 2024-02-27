761c2897e6a3357d83fa5dca8cca369a2313243b
Furukawa, Atsushi <atsushifx@gmail.com>
2024-02-27 16:32:35 +0900

    modify: git/config: delta用に設定を追加 add: thunderbird/signatur: E-mail用signatureを追加 add: ocaml/init.ml: ocaml用初期設定ファイルを追加 chore: powershell/$profile: Ocaml用に環境変数設定部を追加

---
+++ windows/.config/ocaml/init.ml
@@ -0,0 +1,17 @@
+(* @(#) ocaml init file *)
+(*
+ * @version	1.0.1
+ * @author	Furukawa, Atsushi <atsushifx@aglabo.com>
+ * @date	2023-03-01
+ * @license	 MIT
+ *
+ * @desc<<
+ *
+ *<<
+ *)
+
+print_string "init ocaml\n";;
+(*
+let print_non_escaped_string ppf = Format.fprintf ppf "\"%s\"";;
+#install_printer print_non_escaped_string;;
+*)
